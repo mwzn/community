169562e823d11f6b3537720721823addd4b081a2
mwzn <zailaiyibao@yeah.net>
2022-04-06 22:34:51 +0800

    add login

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,15 +1,12 @@
 package life.majiang.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String hello() {
+    public String index() {
         return "index";
     }
 }
